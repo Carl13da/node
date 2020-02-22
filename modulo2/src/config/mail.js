4547aaef58fc9950650aa3c54cde9af1875c2ae5
Carlos Santos <carloshenrique0322@gmail.com>
2020-02-22 13:28:15 -0300

    creating mail helper and appointment controller

---
+++ modulo2/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e8d86b642e84b6',
+    pass: 'a36dcbe1e8e239',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};

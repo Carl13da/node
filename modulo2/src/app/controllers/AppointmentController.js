b897a8625b4d5b7a84fa952704cd6afcc81918dc
Carlos Santos <carloshenrique0322@gmail.com>
2020-02-22 14:40:24 -0300

    inserting exception handler

--- modulo2/src/app/controllers/AppointmentController.js
+++ modulo2/src/app/controllers/AppointmentController.js
@@ -19,7 +19,7 @@ class AppointmentController {
         cancelled_at: null,
       },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
